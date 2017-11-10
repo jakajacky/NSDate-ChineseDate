25d8e6784d5f301b4c5dfc10d02820d26b3e7e1a
jakajacky <zhangxiaoqiangjoe@gmail.com>
2017-11-10 16:35:50 +0800

    中文格式日期

---
+++ NSDate+ChineseDate.h
@@ -0,0 +1,16 @@
+//
+//  NSDate+ChineseDate.h
+//  ChineseDateDemo
+//
+//  Created by JackXu on 16/5/8.
+//  Copyright © 2016年 HY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (ChineseDate)
+
+-(NSString*)getWeekdayString;
+-(NSString*)getChineseCalendar;
+
+@end
